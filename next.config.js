b5ad313e53b68187876b95badcb4f4b854f0dfb4
iceboy1406 <ekodoko880@gmail.com>
2022-07-02 03:04:49 +0700

    edit image loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ['pixabay.com', 'cdn.pixabay.com'],
-    loader: 'cloudinary'
-  }
+    reactStrictMode: false,
+    images: {
+        domains: ['pixabay.com', 'cdn.pixabay.com'],
+        loader: 'akamai',
+        path: '',
+    },
 }
 
 module.exports = nextConfig
